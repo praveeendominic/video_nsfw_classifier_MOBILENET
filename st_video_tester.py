33c37cf234f2cce0996fb16612a7d43f2139dd0c
praveeendominic <61731613+praveeendominic@users.noreply.github.com>
2022-12-03 16:34:36 -0500

    video nudity filter

--- st_video_tester.py
+++ st_video_tester.py
@@ -1,4 +1,3 @@
-
 import streamlit as st
 import os
 
@@ -20,7 +19,7 @@ model=predict.load_model(model_path)
 st.header('VIDEO NUDITY DETECTOR')
 st.write("Please select a video for nudity check.")
 
-video_uploaded=st.file_uploader(label= "Please upload a file",accept_multiple_files=False, type = ['mp4'])
+video_uploaded=st.file_uploader(label= "Please upload a file",accept_multiple_files=False, type = ['mp4','.wmv'])
 if video_uploaded is not None:
     st.video(video_uploaded)
 
@@ -33,35 +32,79 @@ if video_uploaded is not None:
         f.write(video_uploaded.getbuffer())
         st.success("File upload complete..")
 
+    
+    import cv2 
+    result=""
+    fres={}
 
-    def video_nsfw_detector(video_path):
-        videodata=cv2.VideoCapture(video_path)
-        flag=''
-        while(videodata.isOpened()):
-            ret, frame = videodata.read()
-            if ret == True:
+    def video_nsfw(video_path):
+    # c=0
+        vidcap = cv2.VideoCapture(video_path) 
+        # flag=''
+        def getFrame(sec,flag=''): 
+            vidcap.set(cv2.CAP_PROP_POS_MSEC,sec*1000) 
+            hasFrames,frame = vidcap.read() 
+            if hasFrames: 
                 im=Image.fromarray(frame).resize((224,224))
                 image = keras.preprocessing.image.img_to_array(im)
                 image /=255
                 image=np.expand_dims(image,axis=0)
                 res=predict.classify_nd(model,image)[0]
-                if ((res.get('porn')>0.7) or (res.get('sexy')>0.7) or (res.get('hentai')>0.5)):
+                if ((res.get('porn')>0.7) or (res.get('sexy')>0.7) or (res.get('hentai')>0.6)):      
                     flag='x'
-                    break
-                else:
-                    flag = ''
-        if flag!='x':
-            result="Video is safe for publishing!"
+                    # print(res)
+                    # cv2_imshow(frame)
+                    hasFrames=False
+                # cv2.waitKey(0)
+                # return (False, flag)
+                # cv2.imwrite("frame "+str(sec)+" sec.jpg", image)     # save frame as JPG file 
+            return (hasFrames, flag)
+            # return hasFrames 
+        sec = 0 
+        frameRate = 3#it will capture image in each 0.5 second 
+        success = getFrame(sec) 
+        # print(success)
+        while success: 
+            sec = sec + frameRate 
+            sec = round(sec, 3) 
+            success, fl = getFrame(sec) 
+            # print(sec)
+            # print(fl)
+            if fl=='x':
+                return "nsfw"
+                # break
         else:
-            result=f"Obscene content detected"
+            return 'safe'
 
-        return result
+
+    # def video_nsfw_detector(video_path):
+    #     videodata=cv2.VideoCapture(video_path)
+    #     flag=''
+    #     while(videodata.isOpened()):
+    #         ret, frame = videodata.read()
+    #         if ret == True:
+    #             im=Image.fromarray(frame).resize((224,224))
+    #             image = keras.preprocessing.image.img_to_array(im)
+    #             image /=255
+    #             image=np.expand_dims(image,axis=0)
+    #             res=predict.classify_nd(model,image)[0]
+    #             if ((res.get('porn')>0.7) or (res.get('sexy')>0.7) or (res.get('hentai')>0.5)):
+    #                 flag='x'
+    #                 break
+    #             else:
+    #                 flag = ''
+    #     if flag!='x':
+    #         result="Video is safe for publishing!"
+    #     else:
+    #         result=f"Obscene content detected"
+
+    #     return result
 
     import time
     with st.spinner('Wait for it...'):
         time.sleep(5)
 
-        result = video_nsfw_detector(fname_u)
+        result = video_nsfw(fname_u)
         # st.write(result)
 
         if result=="Video is safe for publishing!":
