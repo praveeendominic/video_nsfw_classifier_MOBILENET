afe7c9e2962564fefdd3547483583f75bd7fbf81
praveeendominic <61731613+praveeendominic@users.noreply.github.com>
2022-12-02 22:23:49 -0500

    video nudity detector app

--- st_video_tester.py
+++ st_video_tester.py
@@ -14,24 +14,25 @@ import skvideo
 #Path('ffmpeg') / 'bin'#
 # Path('ffmpeg') / 'bin' #
 import os.path
-ffmpeg_path_os = os.path.join('ffmpeg','bin')
-ffmpeg_path = Path('ffmpeg') / 'bin'
+# ffmpeg_path_os = os.path.join('ffmpeg','bin')
+# ffmpeg_path = Path('ffmpeg') / 'bin'
 
-# ffmpeg_path = 'ffmpeg\bin'#Path('ffmpeg') / 'bin'
-# ffmpeg_path = "C:\\code\\justo_nudity_classifier\\ffmpeg\\bin"
-# skvideo.setFFmpegPath( ffmpeg_path)
+# # ffmpeg_path = 'ffmpeg\bin'#Path('ffmpeg') / 'bin'
+# # ffmpeg_path = "C:\\code\\justo_nudity_classifier\\ffmpeg\\bin"
+# skvideo.setFFmpegPath(str(ffmpeg_path))
 
 import skvideo.io
+import cv2
 
 
 result=''
 model_path = Path('model_mobilenet')/ 'saved_model.h5'
 model=predict.load_model(model_path)
 
-st.write(model_path)
-st.write(model_path)
-st.write(ffmpeg_path_os)
-st.write(ffmpeg_path)
+# st.write(model_path)
+# st.write(model_path)
+# st.write(ffmpeg_path_os)
+# st.write(ffmpeg_path)
 
 st.header('VIDEO NUDITY DETECTOR')
 st.write("Please select a video for nudity check.")
@@ -56,35 +57,31 @@ if video_uploaded is not None:
 
 
     def video_nsfw_detector(video_path):
-        videodata = skvideo.io.vread(video_path)
+        videodata=cv2.VideoCapture(video_path)
         flag=''
-        frame_num=0
-        for i in range(len(videodata)):
-            frame = videodata[i]
-            im=Image.fromarray(frame).resize((224,224))
-            image = keras.preprocessing.image.img_to_array(im)
-            image /=255
-            image=np.expand_dims(image,axis=0)
-
-            res=predict.classify_nd(model,image)[0]
-            # print(res)
-            if ((res.get('porn')>0.7) or (res.get('sexy')>0.7) or (res.get('hentai')>0.5)):
-                flag='x'
-                frame_num=i
-                # print(f"@frame: {i}")
-                # print(res)
-                # print("Obscene content detected!")
-                # cv2.imshow(frame)
-                break
-            
+        while(videodata.isOpened()):
+            ret, frame = videodata.read()
+            if ret == True:
+                im=Image.fromarray(frame).resize((224,224))
+                image = keras.preprocessing.image.img_to_array(im)
+                image /=255
+                image=np.expand_dims(image,axis=0)
+                res=predict.classify_nd(model,image)[0]
+                if ((res.get('porn')>0.7) or (res.get('sexy')>0.7) or (res.get('hentai')>0.5)):
+                    flag='x'
+                    break
+                else:
+                    flag = ''
         if flag!='x':
             # print("Video is safe for publishing!")
             result="Video is safe for publishing!"
         else:
             # print(f'Obscene content detected! at frame {frame_num}')
-            result=f"Obscene content detected @ frame {frame_num}"
+            result=f"Obscene content detected"
 
         return result
+
+
 
     # result = video_nsfw_detector(fname_u)
     # st.write(result)
--- video_nudity_detector1.py
+++ video_nudity_detector1.py
@@ -16,13 +16,63 @@ import pathlib
 from pathlib import Path
 
 import skvideo
-import os.path
-ffmpeg_path_os = os.path.join('ffmpeg','bin')
-ffmpeg_path_pthlib = Path('ffmpeg') / 'bin'
-
-print(ffmpeg_path_os)
-print(ffmpeg_path_pthlib)
-model_path = Path('model_mobilenet')/ 'saved_model.h5'
-print(model_path)
-skvideo.setFFmpegPath(ffmpeg_path_os)
 import skvideo.io
+import os.path
+import numpy as np
+from PIL import Image
+from tensorflow import keras
+# ffmpeg_path_os = os.path.join('ffmpeg','bin')
+# ffmpeg_path_pthlib = Path('ffmpeg') / 'bin'
+
+# print(ffmpeg_path_os)
+# print(ffmpeg_path_pthlib)
+# model_path = Path('model_mobilenet')/ 'saved_model.h5'
+# print(model_path)
+# skvideo.setFFmpegPath(ffmpeg_path_os)
+# import skvideo.io
+
+
+# videodata = skvideo.io.vread("C:\\code\\video nudity detector app\\tmp.mp4")
+
+import sys
+import skvideo.io
+import cv2
+# vd=skvideo.io.vreader("C:\\code\\video nudity detector app\\tmp.mp4")
+
+vid = cv2.VideoCapture("C:\\code\\video nudity detector app\\tmp.mp4")
+while(True):
+    ret, frame = vid.read()
+    im=Image.fromarray(frame).resize((224,224))
+    image = keras.preprocessing.image.img_to_array(im)
+    image /=255
+    image=np.expand_dims(image,axis=0)
+    print(image.shape)
+
+    # print(frame)
+    # print(frame.shape)
+    break
+
+
+# for frame in vd:
+#     print(frame.shape)
+#     im=Image.fromarray(frame).resize((224,224))
+#     image = keras.preprocessing.image.img_to_array(im)
+#     image /=255
+#     image=np.expand_dims(image,axis=0)
+#     print(image.shape)
+#     break
+
+# flag=''
+# frame_num=0
+# for i in range(len(videodata)):
+#     frame = videodata[i]
+#     print(frame)
+#     print(frame.shape)
+
+    # im=Image.fromarray(frame).resize((224,224))
+#     image = keras.preprocessing.image.img_to_array(im)
+#     image /=255
+#     image=np.expand_dims(image,axis=0)
+#     print(image.shape)
+# #     break
+
